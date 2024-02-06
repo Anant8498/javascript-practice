d57bf18aaa6f7a8bb69d8b8700fdb64dbab3dfd1
Anant8498 <ananttyagi.421@gmail.com>
2024-02-06 21:50:57 +0530

    initial

---
+++ if-else.js
@@ -0,0 +1,96 @@
+// if (condition1) {
+//     //  block of code to be executed if condition1 is true
+//   } else if (condition2) {
+//     //  block of code to be executed if the condition1 is false and condition2 is true
+//   } else {
+//     //  block of code to be executed if the condition1 is false and condition2 is false
+//   }
+
+// 1. Write a program to find maximum between two given numbers.
+
+// let a =prompt("enter first number:");
+// let b =prompt("enter second number:");
+
+// // let a =4;
+// // let b =7;
+
+// if(a>b){
+//     console.log("a is greater");
+// }
+
+// else{
+//     console.log("b is greater");
+// }
+
+// 2. Write a program to check whether given no is even or odd.
+
+// let num = 0;
+
+// if (num === 0) {
+//   console.log("num is zero");
+// } else if (num % 2 == 0) {
+//   console.log("num is even");
+// } else {
+//   console.log("num is odd");
+// }
+
+
+
+// 3. Write a program to find maximum between three numbers.
+
+// let a=3;
+// let b=40;
+// let c=5;
+
+// if(a>b&&a>c){
+
+//     console.log("a is greatest");
+
+// }
+// else if(b>a&&b>c){
+
+//     console.log("b is greatest");
+
+// }
+
+// else{
+//     console.log("c is greatest");
+
+// }
+
+// 4. Write a program to check whether a number is negative positive or zero.
+
+// let a="-1";
+
+// if(a===0){
+
+//     console.log("a is zero");
+
+// }
+// else if(a<0){
+
+//     console.log("a is negative");
+
+// }
+
+// else {
+//     console.log("a is positive");
+
+// }
+
+// 5. check whether the number is odd or even or zero
+
+// let num = 0;
+
+// if (num % 2 == 0) {
+//   if (num === 0) {
+//     console.log("num is zer0");
+//   } else {
+//     console.log("num is even");
+//   }
+
+//   // } else if (num ===0) {
+//   //   console.log("num is zero");
+// } else {
+//   console.log("num is odd");
+// }
