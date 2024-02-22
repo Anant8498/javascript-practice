a7c8ea445d5d4abd444f08143b86df7ec10a533b
Anant8498 <ananttyagi.421@gmail.com>
2024-02-23 00:46:08 +0530

    more added

--- if-else.js
+++ if-else.js
@@ -101,3 +101,149 @@
 // } else {
 //   console.log("num is odd");
 // }
+
+// 8. Write a program to input angles of a triangle and check whether triangle is valid or not.
+
+// const angle_1 = parseFloat(prompt("Enter your 1st angle:"));
+// const angle_2 = parseFloat(prompt("Enter your 2nd angle:"));
+// const angle_3 = parseFloat(prompt("Enter your 3rd angle:"));
+
+// let sum = angle_1 + angle_2 + angle_3;
+
+// if (sum === 180 && angle_1 > 0 && angle_2 > 0 && angle_3 > 0) {
+//   console.log("triangle is valid");
+// } else {
+//   console.log("triangle is invalid");
+// }
+
+// 9. Write a program to input sides of a triangle and check whether triangle is valid or not.
+
+//  const side_1 = parseFloat(prompt("Enter your 1st side:"));
+//  const side_2 = parseFloat(prompt("Enter your 2nd side:"));
+//  const side_3 = parseFloat(prompt("Enter your 3rd side:"));
+
+//  if(side_1+side_2 > side_3 && side_1+side_3 > side_2 && side_2 + side_3 > side_1){
+
+//     console.log("triangle is valid");
+//  }
+// else{
+//     console.log("triangle is invalid");
+// }
+
+// 10. Write a program to check whether the triangle is equilateral,isoceles or scalene triangle.
+
+//  const side_1 = parseFloat(prompt("Enter your 1st side:"));
+//  const side_2 = parseFloat(prompt("Enter your 2nd side:"));
+//  const side_3 = parseFloat(prompt("Enter your 3rd side:"));
+
+// if(side_1!=side_2 && side_2!= side_3 && side_1 != side_3){
+//     console.log("triangle is scalene")
+// }
+
+// else if(side_1===side_2 && side_2=== side_3 && side_1 === side_3){
+
+//     console.log("triangle is equivalent")
+// }
+
+// // else if (side_1===side_2 || side_2=== side_3 || side_1 === side_3){
+
+// //     console.log("triangle is isoceles ")
+
+// else{
+//     console.log("triangle is isoceles ")}
+
+// 11. Write a program to read the age of the candidate and determine whether he is eligible to cast his/her vote.
+
+// let age = parseFloat(prompt("enter your age:"))
+
+// if(age === 18 || age>18){
+//     console.log("eligible to vote")
+// }
+
+// else{
+//     console.log("not eligible for vote")
+// }
+
+// 12. Write a program to check whether the input character is alphabet,digit or special character.
+
+// let ch = prompt("enter your character:");
+
+// if ((ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z")) {
+//   console.log("character is alphabet ");
+// } else if (ch >= "0" && ch <= "9") {
+//   console.log("character is digit");
+// } else {
+//   console.log("character is a special character ");
+// }
+
+// 13. Write a program to check whether the input character is vowel or consonant.
+
+// let ch = prompt("enter your character:");
+// if (((ch === "a"|| ch ==="e"|| ch ==="i"|| ch ==="o"|| ch ==="u") ||( ch === "A"||ch ==="E"|| ch ==="I"||ch ==="O"||ch === "U"))) {
+//   console.log("given character is a vowel");
+// } else {
+//   console.log("given character is not a vowel");
+// }
+
+
+
+// 16. Write a program to check whether a number is divisible by 5 and 11 Or not. 
+ 
+// let num =55;
+
+// if (num % 5==0 && num % 11 ==0){
+//     console.log("num is divisible by 5 and 11");
+// }
+// else {
+//     console.log("num is not divisible by 5 and 11");
+// }
+
+// 17. Write a program to check whether a character is upper case Or lower case alphabet. 
+
+// let ch = prompt("enter your character:");
+
+// if (ch >= 'a' && ch <='z'){
+//     console.log("character is lower case");
+
+// }
+// else if (ch >= 'A' && ch <='Z'){
+//     console.log("character is upper case ")
+// }
+// else {
+//     console.log("character is other than alphabet")
+// }
+
+// 18. Write a program to find profit or loss. 
+
+// let cost_price =1500;
+// let sell_price= 1000;
+
+// if ((sell_price - cost_price)>=0){
+
+//     console.log(" you are in profit");
+
+// }
+0
+// else{
+//     console.log("you are in loss")
+// }
+
+
+
+// 19. Write a program to print no of days in a month. 
+
+
+// let month = 6;
+
+// if (month === 1 || month === 3 || month === 5 || month === 7 || month === 9 || month === 11) {
+//     console.log("The days in this month is 31 days");
+// }
+// else if (month === 4 || month === 6 || month === 8 || month === 10 || month === 12) {
+//     console.log("The days in this month is 30 days");
+// }
+// else if (month === 2) {
+//     console.log("The days in this month is 29 days");
+// }
+// else {
+//     console.log("Enter a number between 1-12");
+// }
--- switch-case.js
+++ switch-case.js
@@ -73,3 +73,98 @@
 //   default:
 //     console.log("invalid integer");
 // }
+
+// 14. Write a program to create Simple Calculator using switch case.(+,-,*,/,% etc.)
+
+// let a = 2;
+// let b = 5;
+//  let operator =  prompt("enter your operator :");
+
+// switch (operator) {
+//   case  "+":
+//     console.log(a + b);
+//     break;
+//   case "-":
+//     console.log(a - b);
+//     break;
+//   case "*":
+//     console.log(a * b);
+//     break;
+//   case "/":
+//     console.log(a / b);
+//     break;
+// }
+
+// 15. Write a program to read temperature in centigrade and display a suitable message according to temperature using switch case as given below:-
+
+// Temperature<0 - Freezing weather
+// Temperature 0-10  - Very Cold weather
+// Temperature 10-20 - Cold weather
+// Temperature 20-30 - Normal in temp
+// Temperature 30-40  - Its hot
+// Temperature >=40 - Its very hot11
+
+// let temperature = -90;
+
+// switch (true) {
+//   case temperature < 0:
+//     console.log("Freezing weather");
+//     break;
+//   case temperature >= 0 && temperature < 10:
+//     console.log("Very Cold weatherr");
+//     break;
+//   case temperature >= 10 && temperature < 20:
+//     console.log("Cold weather");
+//     break;
+//   case temperature >= 20 && temperature < 30:
+//     console.log("Normal in temp");
+//     break;
+//   case temperature >= 30 && temperature < 40:
+//     console.log("Its hot");
+//     break;
+//   case temperature >=40:
+//     console.log("Its very hot11");
+//     break;
+// }
+
+// 20. Write a program  to accept a grade and declare the equivalent description :
+
+// Grade	Description
+// E	Excellent
+// V	Very Good
+// G	Good
+// A	Average
+// F	Fail
+
+// let grade = prompt("enter your grade:");
+
+// switch (grade) {
+//   case "A" :
+//   case "a":
+//     console.log("average");
+
+//     break;
+
+//   case "F" :
+//   case "f":
+//     console.log("fail");
+
+//     break;
+
+//   case "E" :
+//   case "e":
+//     console.log("excellent");
+
+//     break;
+
+//   case "V" :
+//   case "v":
+//     console.log("very good");
+
+//     break;
+//   case "G" :
+//   case "g":
+//     console.log("good");
+
+//     break;
+// }
